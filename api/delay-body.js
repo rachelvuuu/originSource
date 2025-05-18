3c0aae776dcae12f7fe5c15d46dda8f426331795
rachelvuuu <96734557+rachelvuuu@users.noreply.github.com>
2025-05-18 15:01:02 +0000

    Update Keep-Alive: Allow Entering dynamic value

--- api/delay-body.js
+++ api/delay-body.js
@@ -1,7 +1,7 @@
 export const config = { api: { bodyParser: false } };
 
 export default async function handler(req, res) {
-  const delay = parseInt(req.headers["x-delay"] || "10", 10);
+  const delay = parseInt(req.headers["x-delay"] || "1", 10);
   let body = Buffer.from([]);
   for await (const chunk of req) {
     await new Promise(resolve => setTimeout(resolve, delay * 1000));
--- api/delay-read.js
+++ api/delay-read.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const time = parseInt(req.query.time || "10", 10);
+  const time = parseInt(req.query.time || "1", 10);
   await new Promise(resolve => setTimeout(resolve, time * 1000));
   res.status(200).send(`Origin Read Delay of ${time}s completed.`);
 }
